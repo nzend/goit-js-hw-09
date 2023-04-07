21b677e0859384f9c6e3a71efacc54e1428338aa
nzend <115723110+nzend@users.noreply.github.com>
2023-04-07 20:12:17 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  bodyBackground: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
+let timeoutId = null;
+refs.stopButton.disabled = true;
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.startButton.disabled = true;
+  refs.stopButton.disabled = false;
+
+  timeoutId = setInterval(() => {
+    refs.bodyBackground.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function onStopClick() {
+  refs.startButton.disabled = false;
+  refs.stopButton.disabled = true;
+  clearInterval(timeoutId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
